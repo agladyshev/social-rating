b0b72257a3c536b2063fd6cd729115c723da9b04
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2018-06-26 19:24:42 +0300

    fix: Fix cron schedule

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const request = require('request-promise-native');
 const cron = require('node-cron');
 const { MongoClient } = require('mongodb');
-const assert = require('assert');
+// const assert = require('assert');
 
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/test?retryWrites=true`;
+const uriTwitter = process.env.TWITTER_URI || 'http://localhost:3001';
+const uriYoutube = process.env.YOUTUBE_URI || 'http://localhost:3002';
+const uriTelegram = process.env.TELEGRAM_URI || 'http://localhost:3003';
 
 // Mongo calls
 
 const fetchStats = async () => {
-  // Collect a list of influencers with twitter accounts
+  // Collect a list of influencers from DB with existing social stats
   let accounts;
   try {
-    const client = await MongoClient.connect(uri, { useNewUrlParser: true });  
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
     const db = client.db(process.env.DB_NAME);
     const col = db.collection('influencers');
     accounts = await col.find(
@@ -40,14 +43,18 @@ const calculateRating = async () => {
 
 // Cron tasks
 
-cron.schedule('* * * *', () => {
+cron.schedule('59 * * * *', () => {
+  // Update social statistics every hour
   const services = {
-    twitter: request('http://localhost:3001'),
-    youtube: request('http://localhost:3002'),
-    telegram: request('http://localhost:3003')
+    twitter: request(uriTwitter),
+    youtube: request(uriYoutube),
+    telegram: request(uriTelegram),
   };
   Promise.all(Object.values(services))
+    // Start parallel async tasks to update stats
+    // Proceed when all tasks done
     .then((statuses) => {
+      // Log if any of services fails
       const failedServices = statuses.reduce((failed, status, index) => {
         if (status !== 'OK') {
           failed.push(Object.keys(services)[index]);
@@ -57,6 +64,7 @@ cron.schedule('* * * *', () => {
       if (!failedServices.length) {
         failedServices.forEach(service => console.log(`${service} service failed`));
       } else {
+      // If all services return OK, calculate social rating
         calculateRating();
       }
     })
